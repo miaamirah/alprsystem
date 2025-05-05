2c64096eafce3a33d6ada855b4f422378e849401
miaamirah <liyana.suhailie@gmail.com>
2025-05-05 19:50:06 +0800

    change

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/loginpage', function () {
+    return view('loginpage');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
